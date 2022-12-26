999bedfa205c2e657dab7654c2c065b71aba54ec
Stefan Stevic <steviss@gmail.com>
2022-12-26 22:53:33 +0100

    fixes

--- home/src/components/ContactUsForm/ContactUsForm.validation.ts
+++ home/src/components/ContactUsForm/ContactUsForm.validation.ts
@@ -3,5 +3,5 @@ import * as yup from 'yup'
 export const CONTACT_US_FORM_SCHEMA = yup.object({
   email: yup.string().email().required(),
   subject: yup.string().required(),
-  content: yup.string().nullable(),
+  content: yup.string().required(),
 })
